6daac0a86f580997916e961d35605ddf578fae75
ogbeiwi <osifohhilary5000@yahoo.com>
2019-04-17 11:31:13 -0400

    Adding application java

---
+++ src/main/java/clinic/programming/Application.java
@@ -0,0 +1,10 @@
+package clinic.programming;
+public class Application {
+public Application(){
+System.out.println("Inside appliaction");
+}
+public static void main(String[] args){
+System.out.println("Application Service is Starting Up");
+Application app=new Application();
+}
+}
